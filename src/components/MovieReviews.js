3b53d0e8ea47481d8e98a398759765905aa73bbf
manel turki <menelturki@gmail.com>
2020-03-19 10:23:53 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,16 @@
-// Code MovieReviews Here
+import React from 'react'
+
+export default function MovieReviews(props){
+        return (
+            <div className="review-list" >
+                {
+                props.reviews.map(mov=>{
+                    return <div className="review">
+                    <img src={mov.multimedia.src} alt=""/>
+                    <p>{mov.display_title}</p>
+                    </div>
+                })
+                }
+            </div>
+        )
+}
